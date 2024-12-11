fbbd53b82e4b9723f82b5712de0cdd57a9439116
librelol <hello@priv.lol>
2024-12-10 22:18:05 -0500

    Forgot to add the new files

---
+++ src/src/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="8" lg="6">
+        <v-card>
+          <v-card-title>
+            <h1>Priv Dev</h1>
+          </v-card-title>
+          <v-card-text>
+            <p>Providing you with the resources and a community to gain online sovereignty</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary">Discover More</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h1 {
+  margin: 0;
+}
+
+p {
+  margin: 0;
+}
+</style>
